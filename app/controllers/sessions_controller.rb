2080ceabe0ff6d0478d225e14588bc57b79bfb46
Marck <mercercountynj@gmail.com>
2016-01-08 10:36:15 -0500

    sessions/users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,13 @@ class SessionsController < ApplicationController
     #process data to compare to registered users
     def create
 		      @user = User.where(email: params[:email]).first      
-        if @user && @user.password == params[:password]                 session[:user_id] = @user.id        
+        if @user && @user.password == params[:password]                 
+            session[:user_id] = @user.id        
             redirect_to home_path
-		       end
+        else
+            flash[:danger] = 'Invalid email/password combination'
+            render 'new'
+		      end
 	   end
 
     
