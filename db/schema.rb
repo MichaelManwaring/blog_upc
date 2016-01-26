db3059a6861d8bcb55d7fcc8554ce5cbbfaf8ccc
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2016-01-26 17:58:11 -0500

    fix jeff issues 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109172208) do
+ActiveRecord::Schema.define(version: 20160126225149) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "c_content"
